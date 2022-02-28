183e116e902c9563533ac79218fc1c5258fc4481
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-02-28 12:05:04 -0800

    add restaurant controller

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,71 @@
+class RestaurantsController < ApplicationController
+    before_action :require_login, except: [:index, :show]
+    before_action :find_restaurant, only: [:show, :edit, :update, :destroy]
+    before_action :current_user
+
+
+    def index
+        @restaurants = Restaurant.all
+    end
+
+    def show
+        @reviews = @restaurant.reviews
+    end
+
+    def ordered
+        @restaurants = Restaurant.all.sort_restaurants
+        render :index
+    end
+
+    def new
+        @restaurant = Restaurant.new
+    end
+
+    def create   
+        @cuisines = ["Mexican", "Asian", "Italian", "American", "Seafood", "Other"]
+        @restaurant = Restaurant.find_by(restaurant_name: params[:restaurant][:restaurant_name])
+            if @restaurant
+                flash[:message] = "A Restaurant with this name already exists, you can either review it or add one with a different name."
+                redirect_to restaurants_path
+            else
+                @restaurant = Restaurant.new(restaurant_params)
+                @restaurant.user_id = session[:user_id]   
+                @restaurant.save                    #where validations happen
+                if !@restaurant.cuisine_id
+                    render :new
+                else
+                    redirect_to restaurant_path(@restaurant)
+                end
+            end
+        end
+
+
+    def edit
+        #already finds the restaurant with before action
+    end
+
+    def update
+        if @restaurant.update(restaurant_params)
+            redirect_to restaurant_path(@restaurant)
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @restaurant.destroy
+        redirect_to user_path(@current_user)
+    end
+
+
+    private
+
+    def restaurant_params
+        params.require(:restaurant).permit(:restaurant_name, :address, :city, :state, :user_id, :cuisine_id, cuisine_attributes: [:name])
+    end
+
+    def find_restaurant
+        @restaurant = Restaurant.find_by(id: params[:id])
+    end
+
+end
