ea669706fbe68552dba61720c9cf4c8464bb6e3b
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-02-28 12:06:17 -0800

    add sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,46 @@
+class SessionsController < ApplicationController
+
+    def home
+    end
+
+    def new
+    end
+
+    def create     #does the user exist in our system
+        if (params[:user][:email]) == "" || (params[:user][:password]) == ""
+            flash[:message] = "Invalid email or password, please try again."
+            render '/sessions/new'
+        else
+            @user = User.find_by(email: params[:user][:email])
+            if @user && @user.authenticate(params[:user][:password ])
+                        #compares your password to the hashed version in password_digest column
+                session[:user_id] = @user.id
+                redirect_to user_path(@user)
+            else
+                flash[:message] = "Sorry, something went wrong. Please try again. "
+                redirect_to login_path
+            end
+        end
+    end
+
+    
+    def destroy
+        session.delete(:user_id)
+        redirect_to "/"
+    end
+    
+    def google
+        @user = User.from_omniauth(auth)
+        session[:user_id] = @user.id
+ 
+        redirect_to user_path(@user)
+    end
+    
+
+    private
+
+    def auth
+        request.env['omniauth.auth']
+    end
+
+end
