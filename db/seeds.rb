7365f342258eea9fe0b1e577298688041ddce7f9
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-02-28 09:55:11 -0800

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: 'Ryan', email: 'rzemlin@gmail.com', password: "")
+User.create(name: 'Greg', email: 'gzemlin1@gmail.com', password: "")
+
+Cuisine.create(name: "Mexican")
+Cuisine.create(name: "Chinese")
+Cuisine.create(name: "American")
+Cuisine.create(name: "Italian")
+Cuisine.create(name: "Seafood")
+
+Restaurant.create(restaurant_name: "Sam's Chowder House")
+Restaurant.create(restaurant_name: "San Jose Taqueria")
+
