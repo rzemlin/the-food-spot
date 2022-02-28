5c9e22e88bb928d9e995ce4bb382f6090c668f29
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-02-28 12:04:07 -0800

    add helper method and resource routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::Base
+    
+    helper_method :current_user, :logged_in?, :require_login, :authorized_to_edit
+
+    private
+
+    def current_user
+        @current_user ||= User.find_by_id(session[:user_id])
+    end
+
+    def logged_in?
+        !!session[:user_id] 
+    end
+
+    def require_login
+        unless logged_in?
+        flash[:message] = "You must be logged in to continue"
+        redirect_to login_path    
+        end
+    end
+
+    def authorized_to_edit
+
+    end
+    
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "sessions#home"
+  get '/login', to: "sessions#new"
+  post 'login', to: "sessions#create"
+  get "/signup", to: "users#new"
+  
+  get 'auth/:provider/callback', to: 'sessions#google'
+  get 'auth/failure', to: redirect('/')
+  
+  delete '/logout', to: "sessions#destroy"
+  
+  get '/restaurants/ordered', to: "restaurants#ordered"
+
+  resources :reviews
+  resources :restaurants do 
+    resources :reviews, only: [:new, :index]
+  end
+  
+  resources :users
 end
