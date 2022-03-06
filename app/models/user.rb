c62df446aae5a6ce9b1bb745504d97eff8f2542f
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-03-06 06:58:11 -0800

    change css styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
     has_many :reviews
     has_many :restaurants, through: :reviews
 
-    #regex expression to validate email
+    #email validation
     validates :email, uniqueness: true, presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i}
     validates :name, presence: true
 
-    #from bcrypt
+    #bcrypt
     has_secure_password
         
 end
