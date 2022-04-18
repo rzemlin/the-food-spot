cc1ded8a0f786efb5c750baf5c2ac8c549c63364
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-04-18 10:32:12 -0700

    fix omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -24,17 +24,25 @@ class SessionsController < ApplicationController
         end
     end
         
+    
+
         def destroy
             session.delete(:user_id)
             redirect_to "/"
         end
         
         def github_omniauth
-            @user = User.from_omniauth(auth)
-            session[:user_id] = @user.id
-            #binding.pry
-            redirect_to 'users/show'
-        end
+    @user =
+      User.find_or_create_by(uid: auth['uid']) do |u|
+        u.name = auth['info']['name']
+        u.email = auth['info']['email']
+        u.image = auth['info']['image']
+      end
+
+    session[:user_id] = @user.id
+
+    render 'users/show/'
+  end
 
 
    private
